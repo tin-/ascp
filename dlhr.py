855e99536148c79222488724a3459218344175a1
TiN <tin-@users.noreply.github.com>
2017-04-12 10:20:43 +0000

    I2C reads working with ASC DLHR now, ID #1387 @1h

---
+++ dlhr.py
@@ -0,0 +1,182 @@
+# Copyright (c) 2017 xDevs.com
+# Author: Illya Tsemenko
+#
+# Based on the ASC Demo code 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import logging
+import time
+import ConfigParser
+
+cfg = ConfigParser.ConfigParser()
+cfg.read('pressure.conf')
+cfg.sections()
+
+# ASC_DLHR default address.
+ASC_DLHR_I2CADDR = 0x29
+
+# ASC_DLHR Commands
+ASC_DLHR_SINGLE_READ_CMD = 0xAA
+ASC_DLHR_AVG2_READ_CMD   = 0xAC
+ASC_DLHR_AVG4_READ_CMD   = 0xAD
+ASC_DLHR_AVG8_READ_CMD   = 0xAE
+ASC_DLHR_AVG16_READ_CMD  = 0xAF
+
+# ASC_DLHR Status bits
+ASC_DLHR_STS_ZERO          = 0b10000000
+ASC_DLHR_STS_PWRON         = 0b01000000
+ASC_DLHR_STS_BUSY          = 0b00100000
+ASC_DLHR_STS_MODE          = 0b00011000
+ASC_DLHR_STS_EEPROM_CHKERR = 0b00000100
+ASC_DLHR_STS_SNSCFG        = 0b00000010
+ASC_DLHR_STS_ALUERR        = 0b00000001
+
+global bus
+
+class ASC_DLHR(object):
+    def __init__(self, mode=ASC_DLHR_I2CADDR, address=ASC_DLHR_I2CADDR, i2c=None,
+                 **kwargs):
+        self._logger = logging.getLogger('Adafruit_BMP.BMP085')
+        # Check that mode is valid.
+        if mode != 0x29:
+            raise ValueError('Unexpected address'.format(mode))
+        self._mode = mode
+        global bus
+        
+        # Create I2C device.
+        if cfg.get('main', 'if_debug', 1) == 'false':
+            if i2c is None:
+                #import Adafruit_GPIO.I2C as I2C
+                #i2c = I2C
+                from smbus import SMBus
+                bus = SMBus(1)
+            #self._device = i2c.get_i2c_device(address, **kwargs)
+        
+        # Load calibration values.
+        #self._load_calibration()
+        #self.t_fine = 0.0
+        
+    def write_cmd(self, cmd):
+        if cfg.get('main', 'if_debug', 1) == 'false':
+            bus.write_i2c_block_data(ASC_DLHR_I2CADDR, cmd, [0x00, 0x00])
+            #self._device.writeRaw8(self.dcmd)
+            time.sleep(0.01)
+            
+            #bus.write_byte(ASC_DLHR_I2CADDR, 0)
+            #self._device.writeRaw8(0x00)
+            #time.sleep(0.01)
+	    
+            #bus.write_byte(ASC_DLHR_I2CADDR, 0)
+            #self._device.writeRaw8(0x00)
+            #time.sleep(0.01)
+	if cfg.get('main', 'if_debug', 1) == 'true':
+	    print "\033[31;1mDebug mode only, command = %X\033[0;39m" % cmd,
+        time.sleep(0.2)
+        return 1
+
+    def chk_busy(self):
+        if cfg.get('main', 'if_debug', 1) == 'false':
+            Status = bus.read_byte(ASC_DLHR_I2CADDR)
+	    #Status = self._device.readRaw8()  # Receive Status byte 
+        if cfg.get('main', 'if_debug', 1) == 'true':
+            Status = 0x40
+        
+        if (Status & ASC_DLHR_STS_BUSY):
+            print "\033[31;1m\r\nPower On status not set!\033[0;39m",
+            return 1 # sensor is busy
+        time.sleep(0.2)
+        return 0     # sensor is ready
+        
+    def read_sensor(self):
+        outb = [0,0,0,0,0,0,0,0]
+    
+        #// wait for completion
+        #while (LOW == digitalRead(EOCPIN))
+        time.sleep(1)
+        self.retry_num = 5
+        while self.retry_num > 0:
+            if self.chk_busy():
+                print "\033[31;1m\r\nSensor is busy!\033[0;39m",
+                time.sleep(0.05)                       # Sleep for 100ms
+            else:
+                self.retry_num = 0
+            self.retry_num = self.retry_num - 1
+        
+        if cfg.get('main', 'if_debug', 1) == 'false':
+            #StatusByte = self._device.readRaw8()  # Receive Status byte 
+            #outb[0] = self._device.readRaw8()     # Receive Pressure 3 data byte
+            #outb[1] = self._device.readRaw8()     # Receive Pressure 2 data byte
+            #outb[2] = self._device.readRaw8()     # Receive Pressure 1 data byte
+            #outb[3] = self._device.readRaw8()     # Receive Temp 3 data byte
+            #outb[4] = self._device.readRaw8()     # Receive Temp 2 data byte
+            #outb[5] = self._device.readRaw8()     # Receive Temp 1 data byte
+    	    #StatusByte = bus.read_byte(ASC_DLHR_I2CADDR)
+	    #outb[0] = bus.read_byte(ASC_DLHR_I2CADDR)
+	    #outb[1] = bus.read_byte(ASC_DLHR_I2CADDR)
+	    #outb[2] = bus.read_byte(ASC_DLHR_I2CADDR)
+	    #outb[3] = bus.read_byte(ASC_DLHR_I2CADDR)
+	    #outb[4] = bus.read_byte(ASC_DLHR_I2CADDR)
+	    #outb[5] = bus.read_byte(ASC_DLHR_I2CADDR)
+	    outb = bus.read_i2c_block_data(ASC_DLHR_I2CADDR, 0, 7)
+            StatusByte = outb[0]
+
+        if cfg.get('main', 'if_debug', 1) == 'true':
+            StatusByte = 0xFF
+            outb[0] = 0x55
+            outb[1] = 0x55
+            outb[2] = 0x55
+            outb[3] = 0x55
+            outb[4] = 0x55
+            outb[5] = 0x55
+        
+        # Check for correct status
+        if (StatusByte & ASC_DLHR_STS_PWRON) == 0:
+            print "\033[31;1m\r\nPower On status not set!\033[0;39m",
+            quit()
+        if (StatusByte & ASC_DLHR_STS_SNSCFG):
+            print "\033[31;1m\r\nIncorrect sensor CFG!\033[0;39m",
+            quit()
+        if (StatusByte & ASC_DLHR_STS_EEPROM_CHKERR):
+            print "\033[31;1m\r\nSensor EEPROM Checksum Failure!\033[0;39m",
+            quit()
+            
+        # Ccnvert Temperature data to degrees C:
+        Tmp = outb[4] << 8
+        Tmp += outb[5]
+        fTemp = float(Tmp)
+        fTemp = (fTemp/65535.0) * 125.0 - 40.0
+        
+        # Convert Pressure to %Full Scale Span ( +/- 100%)
+        Prs = (outb[1] <<16) + (outb[2]<<8) + (outb[3])
+        Prs -= 0x7FFFFF
+        fPress = (float(Prs))/(float(0x800000))
+        fPress *= 100.0
+    
+        print "Status: 0x%X " % StatusByte,
+        print "Pressure: %4.5f %%FSS " % fPress,
+        print "Temperature: %3.2f 'C " % fTemp,
+        
+        print " 0x%04X %04X %04X " % (outb[1], outb[2], outb[3]),
+        
+        Tmp = outb[4] << 16;
+        Tmp += outb[5] << 8;
+        Tmp += outb[6];
+        
+        print " %X " % Tmp
+        return 0
---
+++ main.py
@@ -0,0 +1,151 @@
+# Copyright (c) 2017 xDevs.com
+# Author: Illya Tsemenko
+#
+# Based on the ASC Demo code 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import imp
+import sys
+import ConfigParser
+
+cfg = ConfigParser.ConfigParser()
+cfg.read('pressure.conf')
+cfg.sections()
+
+if cfg.get('dut', 'dut', 1) == 'asc_dlhr':
+    if cfg.get('main', 'interface', 1) == 'i2c':
+        # ASC DLHR sensor with I2C interface
+        from dlhr import *
+        sensorhw = imp.load_source('dlhr', 'dlhr.py')
+        sensor = sensorhw.ASC_DLHR(mode=ASC_DLHR_I2CADDR)
+
+menu = """\033[1;32m[0] - Help guide
+[1] - Detect instruments
+[2] - Initialize instruments
+[3] - Select DUT
+[4] - Run PT procedure on DUT
+\033[0;44m[5] - Do performance test and autogenerate HTML report\033[1;32;49m
+[6] - Generate HTML report
+[7] - a: Read part Pressure & Temp
+[8] - b: Read part 2x oversampled Pressure & Temp
+[9] - c: Read part 4x oversampled Pressure & Temp
+[10] - d: Read part 8x oversampled Pressure & Temp
+[11] - e: Read part 16x oversampled Pressure & Temp
+[12] - U: Toggle Warmup cycles on/off
+[X] - Quit\033[0;39m
+"""
+
+help = """\033[0;44m
+This is help! Very good help!
+To know more - visit https://xdevs.com site\033[0;39m
+"""
+
+sys.stdout.write("\033[0;36m  Pressure sensors toolkit CLI \r\n  Using NI GPIB adapter and linux-gpib library \r\n\033[0;41m Target platform: Raspberry Pi B 3+\033[0;39m\r\n")
+
+def GetChar(Block=True):
+    if Block or select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+        return sys.stdin.read(1)
+    raise error('NoChar')
+
+c = "*"
+h = ("*")
+while True:
+    print c + h*78 + c
+    print menu
+    inputa = raw_input(" "*3 + "Input example: ")
+    print c + h*78 + c
+
+    if (inputa == "0"):
+        print help
+
+    if (inputa == "1"):
+        with open('calkit.conf', 'w') as configfile:
+            cfg.write(configfile)
+        print ("Detecting function not written yet")
+
+    if (inputa == "2"):
+        print ("Initialize instruments not written yet")
+
+    if (inputa == "3"):
+        hp3458_setup()
+        print ("Select DUT submenu not written yet")
+
+    if (inputa == "4"):
+        print ("Performance verification procedure on DUT written yet")
+
+    if (inputa == "5"):
+        print ("Generating HTML report..\033[0;49m")
+        cal_report()
+        print ("All done, aye!\033[0;39m")
+
+    if (inputa == "6"):
+        print ("Generating HTML report..\033[0;49m")
+        mfc_report()
+        print ("All done, aye!\033[0;39m")
+
+    if (inputa == "7"):
+        print ("Reading sensor Pressure/temp..\033[0;49m")
+        sensor.write_cmd(ASC_DLHR_SINGLE_READ_CMD)
+        sensor.read_sensor()
+        print ("All done, aye!\033[0;39m")
+
+    if (inputa == "8"):
+        print ("Reading AVG2 sensor Pressure/temp..\033[0;49m")
+        sensor.write_cmd(ASC_DLHR_AVG2_READ_CMD)
+        sensor.read_sensor()
+        print ("All done, aye!\033[0;39m")
+
+    if (inputa == "9"):
+        print ("Reading AVG4 sensor Pressure/temp..\033[0;49m")
+        sensor.write_cmd(ASC_DLHR_AVG4_READ_CMD)
+        sensor.read_sensor()
+        print ("All done, aye!\033[0;39m")
+
+    if (inputa == "10"):
+        print ("Reading AVG8 sensor Pressure/temp..\033[0;49m")
+        sensor.write_cmd(ASC_DLHR_AVG8_READ_CMD)
+        sensor.read_sensor()
+        print ("All done, aye!\033[0;39m")
+
+    if (inputa == "11"):
+        print ("Reading AVG16 sensor Pressure/temp..\033[0;49m")
+        sensor.write_cmd(ASC_DLHR_AVG16_READ_CMD)
+        sensor.read_sensor()
+        print ("All done, aye!\033[0;39m")
+
+    if (inputa == "12"):
+        print ("Warmup sensor..\033[0;49m")
+        DoReads = 0
+        print "Reading sensor. Press any key to abort"
+        while DoReads != 1:
+            sensor.write_cmd(ASC_DLHR_AVG2_READ_CMD)
+            sensor.read_sensor()
+            time.sleep(0.05)
+            DoReads = 0
+            
+            #data = GetChar()
+            #if data:
+            #    DoReads = 0
+            #    break
+        
+        print ("All done, aye!\033[0;39m")
+        
+    if (inputa == "X" or inputa == "x" or inputa == "Q" or inputa == "q"):
+        print ("Bye-bye!")
+        quit()
